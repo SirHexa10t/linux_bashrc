ddf7c6275e1ff4ec36da9750dc15c95d83591855
SirHexalot <sirhexal@gmail.com>
2023-03-07 10:08:03 +0200

    Added hashmap functions
    Also improved the command listing function and ordered a bit of some other stuff

--- unlimited_script_works/_confirm_loaded.py
+++ unlimited_script_works/_confirm_loaded.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
 """
-Returns success. Basically just for checking that another script successfully reached and ran this file
+Returns success. Basically just for checking that another script successfully reached and ran this file. Add "-v" as argument for textual success reply
 """
+
+import sys
+
+if "-v" in sys.argv:
+    print("success")
+
 
 exit(0)
 
